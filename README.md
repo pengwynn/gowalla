8744ff9318aa9af02e20090ccaf3dd48bd7d04d9
Wynn Netherland <wynn.netherland@gmail.com>
2010-06-26 19:18:11 -0500

    docs

--- README.md
+++ README.md
@@ -13,33 +13,66 @@ Be sure and get your API key: [http://gowalla.com/api/keys](http://gowalla.com/a
     
 ## Usage
 
-### Instantiate a client
+### Instantiate a client (Basic Auth)
 
-    >> gowalla = Gowalla::Client.new(:username => 'pengwynn', :password => 'somepassword', :api_key => 'your_api_key')
+    gowalla = Gowalla::Client.new(:username => 'pengwynn', :password => 'somepassword', :api_key => 'your_api_key')
     
 ### or configure once
 
-    >> Gowalla.configure do |config|
-    >>   config.api_key = 'your_api_key'
-    >>   config.username = 'pengwynn'
-    >>   config.password = 'somepassword'
-    >> end
-    >> gowalla = Gowalla::Client.new
+    Gowalla.configure do |config|
+      config.api_key = 'your_api_key'
+      config.username = 'pengwynn'
+      config.password = 'somepassword'
+    end
+    gowalla = Gowalla::Client.new
+    
+### Instantiate a client (OAuth2)
+
+For OAuth2, you'll need to specify a callback URL when you [set up your app and get your API keys](http://gowalla.com/api/keys).
+
+Let's create the client in a protected method in our controller:
+
+    protected
+      def client    
+        Gowalla::Client.new (
+          :api_key     => "your_api_key",
+          :api_secret  => "your_api_secret",
+          :access_token => session[:access_token]
+        )
+      end
+
+We need to get an access token. Perhaps in an a before filter where you need to access Gowalla:
+    
+    redirect(@client.web_server.authorize_url(:redirect_uri => redirect_uri, :state => 1))
+    
+You'll need a callback route to catch the code coming back from Gowalla after a user grants you access and this must match what you specified when you created your app on Gowalla:
+
+    get '/auth/gowalla/callback' do
+      session[:access_token] = client.web_server.get_access_token(params[:code], :redirect_uri => redirect_uri).token
+
+      if session[:access_token]
+        redirect '/auth/gowalla/test'
+      else
+        "Error retrieving access token."
+      end
+    end
+    
+Now that we have an access token we can use the client to make calls, just like we would with Basic Auth. You can checkout a basic [Sinatra example](http://gist.github.com/454283).
     
 #### Examples
 
-    >> gowalla.user('pengwynn')
-    => <#Hashie::Mash accept_url="/friendships/accept?user_id=1707" activity_url="/users/1707/events" bio="Web designer and Ruby developer." events_url="/users/1707/events" fb_id=605681706 first_name="Wynn" friends_count=27 friends_only=false friends_url="/users/1707/friends" hometown="Aubrey, TX" image_url="http://s3.amazonaws.com/static.gowalla.com/users/1707-standard.jpg?1262011383" is_friend=false items_count=5 items_url="/users/1707/items" last_name="Netherland" last_visit=<#Hashie::Mash comment="Closing every account I have " created_at="2010/01/26 15:31:46 +0000" spot=<#Hashie::Mash image_url="http://static.gowalla.com/categories/186-standard.png" name="Bank Of America" small_image_url="http://static.gowalla.com/categories/186-small-standard.png" url="/spots/164052">> name="Wynn Netherland" pins_count=3 pins_url="/users/1707/pins" reject_url="/friendships/reject?user_id=1707" request_url="/friendships/request?user_id=1707" stamps_count=15 stamps_url="/users/1707/stamps" top_spots_url="/users/1707/top_spots" twitter_username="pengwynn" url="/users/1707" username="pengwynn" vaulted_kinds_count=0 visited_spots_count=15 website="http://wynnnetherland.com">
+    gowalla.user('pengwynn')
+    => <#Hashie::Mash accept_url="/friendships/accept?user_id=1707" activity_url="/users/1707/events" bio="Web designer and Ruby developer." events_url="/users/1707/events" fb_id=605681706 first_name="Wynn" friends_count=27 friends_only=false friends_url="/users/1707/friends" hometown="Aubrey, TX" image_url="http://s3.amazonaws.com/static.gowalla.com/users/1707-standard.jpg?1262011383" is_friend=false items_count=5 items_url="/users/1707/items" last_name="Netherland" last_visit=<#Hashie::Mash comment="Closing every account I have " created_at="2010/01/26 15:31:46 +0000" spot=<#Hashie::Mash image_url="http://static.gowalla.com/categories/186-standard.png" name="Bank Of America" small_image_url="http://static.gowalla.com/categories/186-small-standard.png" url="/spots/164052"name="Wynn Netherland" pins_count=3 pins_url="/users/1707/pins" reject_url="/friendships/reject?user_id=1707" request_url="/friendships/request?user_id=1707" stamps_count=15 stamps_url="/users/1707/stamps" top_spots_url="/users/1707/top_spots" twitter_username="pengwynn" url="/users/1707" username="pengwynn" vaulted_kinds_count=0 visited_spots_count=15 website="http://wynnnetherland.com">
     
 
-#### Details for the current user
+Details for the current user
 
-    >> gowalla.user
+    gowalla.user
     
-#### Details for another user
+Details for another user
 
-    >> gowalla.user('bradleyjoyce')
-    >> gowalla.user(1707)
+    gowalla.user('bradleyjoyce')
+    gowalla.user(1707)
     
 
 
--- lib/gowalla/client.rb
+++ lib/gowalla/client.rb
@@ -17,15 +17,28 @@ module Gowalla
       connection.basic_auth(@username, password) unless @api_secret
     end
     
+    # Retrieve information about a specific user
+    #
+    # @param [String] user_id (authenticated basic auth user) User ID (screen name)
+    # @return [Hashie::Mash] User info
     def user(user_id=nil)
       user_id ||= username
       handle_response(connection.get("/users/#{user_id}"))
     end
 
+    # Retrieve information about a specific item
+    #
+    # @param [Integer] id Item ID
+    # @return [Hashie::Mash] item info
     def item(id)
       handle_response(connection.get("/items/#{id}"))
     end
 
+    # Retrieve a list of the stamps the user has collected
+    #
+    # @param [String] user_id (authenticated basic auth user) User ID (screen name)
+    # @param [Integer] limit (20) limit per page
+    # @return [Hashie::Mash] stamps info
     def stamps(user_id=self.username, limit=20)
       response = connection.get do |req|
         req.url "/users/#{user_id}/stamps", :limit => limit
@@ -33,26 +46,52 @@ module Gowalla
       handle_response(response).stamps
     end
 
+    # Retrieve a list of spots the user has visited most often
+    #
+    # @param [String] user_id (authenticated basic auth user) User ID (screen name)
+    # @return [Hashie::Mash] item info
     def top_spots(user_id=self.username)
       handle_response(connection.get("/users/#{user_id}/top_spots")).top_spots
     end
 
+    # Retrieve information about a specific trip
+    #
+    # @param [Integer] trip_id Trip ID
+    # @return [Hashie::Mash] trip info
     def trip(trip_id)
       handle_response(connection.get("/trips/#{trip_id}"))
     end
     
+    # Retrieve information about a specific spot
+    #
+    # @param [Integer] spot_id Spot ID
+    # @return [Hashie::Mash] Spot info
     def spot(spot_id)
       handle_response(connection.get("/spots/#{spot_id}"))
     end
     
+    # Retrieve a list of check-ins at a particular spot. Shows only the activity that is visible to a given user.
+    #
+    # @param [Integer] spot_id Spot ID
+    # @return [Hashie::Mash] Spot info
     def spot_events(spot_id)
       handle_response(connection.get("/spots/#{spot_id}/events")).activity
     end
     
+    # Retrieve a list of items available at a particular spot
+    #
+    # @param [Integer] spot_id Spot ID
+    # @return [Hashie::Mash] Spot info
     def spot_items(spot_id)
       handle_response(connection.get("/spots/#{spot_id}/items")).items
     end
     
+    # Retrieve a list of spots within a specified distance of a location
+    #
+    # @option options [Float] :latitude Latitude of search location
+    # @option options [Float] :longitude Longitude of search location
+    # @option options [Integer] :radius Search radius (in meters)
+    # @return [Hashie::Mash] spots info
     def list_spots(options={})
       query = format_geo_options(options)
       response = connection.get do |req|
@@ -61,7 +100,9 @@ module Gowalla
       handle_response(response).spots
     end
     
-
+    # List of trips
+    #
+    # @return [<Hashie::Mash>] trip info
     def trips(options={})
       if user_id = options.delete(:user_id)
         options[:user_url] = "/users/#{user_id}"
@@ -73,18 +114,31 @@ module Gowalla
       handle_response(response).trips
     end
 
+    # Lists all spot categories
+    #
+    # @return [<Hashie::Mash>] category info
     def categories
       handle_response(connection.get("/categories")).spot_categories
     end
     
+    # Retrieve information about a specific category
+    #
+    # @param [Integer] id Category ID
+    # @return [Hashie::Mash] category info
     def category(id)
       handle_response(connection.get("/categories/#{id}"))
     end
     
+    # Check for missing access token
+    #
+    # @return [Boolean] whether or not to redirect to get an access token
     def needs_access?
       @api_secret and @access_token.to_s == ''
     end
     
+    # Raw HTTP connection, either Faraday::Connection or OAuth2::AccessToken
+    #
+    # @return [OAuth2::Client]
     def connection
       
       if api_secret
@@ -98,6 +152,9 @@ module Gowalla
       end
     end
     
+    # Provides raw access to the OAuth2 Client
+    #
+    # @return [OAuth2::Client] 
     def oauth_client
       if @oauth_client
         @oauth_client
@@ -118,6 +175,7 @@ module Gowalla
 
     private
     
+      # @private
       def format_geo_options(options={})
         options[:lat] = "+#{options[:lat]}" if options[:lat].to_i > 0
         options[:lng] = "+#{options[:lng]}" if options[:lng].to_i > 0
@@ -127,13 +185,15 @@ module Gowalla
         options
       end
       
+      # @private
       def default_headers
         headers = {
           :accept =>  'application/json', 
           :user_agent => 'Ruby gem'
         }
       end
-    
+      
+      # @private
       def handle_response(response)
         case response.status
         when 200
